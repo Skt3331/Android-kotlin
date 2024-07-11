89356f1ef9d1cae5e25454535051de13f1307fff
Saurav Temgire <144504510+Skt3331@users.noreply.github.com>
2024-07-11 16:30:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/SplashScrren.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScrren : AppCompatActivity() {
+    private val SPLASH_SCREEN_DELAY: Long = 2000 // 2 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_scrren)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Start the main activity after a delay
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScrren, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Optional: close the splash screen activity
+        }, SPLASH_SCREEN_DELAY)
+    }
+}
